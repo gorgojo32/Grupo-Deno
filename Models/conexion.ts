5b893e53e807ede529a466ad65c2a8ab42763256
DonGavo <cesar_dgaviria@soy.sena.edu.co>
2025-02-26 16:46:05 -0500

    Terminar Conexion (Falta nombre de db)

--- Models/conexion.ts
+++ Models/conexion.ts
@@ -1,2 +1,8 @@
 import {Client} from "../dependencies/dependencias.ts";
 
+export const Conexion = await new Client().connect({
+    hostname: "localhost",
+    username: "root",
+    db: "",
+    password: "",
+})
