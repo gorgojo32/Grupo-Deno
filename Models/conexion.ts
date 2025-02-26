32eee1daebd3ab3c7689e15304424b85ee12229f
DonGavo <cesar_dgaviria@soy.sena.edu.co>
2025-02-26 17:12:10 -0500

    Pruba 3

--- Models/conexion.ts
+++ Models/conexion.ts
@@ -2,6 +2,7 @@ import {Client} from "../dependencies/dependencias.ts";
 
 // Julio Se la come
 // Prueba 2
+// Prueba 3
 
 export const Conexion = await new Client().connect({
     hostname: "localhost",
