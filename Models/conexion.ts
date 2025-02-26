2ac7dc5abcf3a372675c67b0e3e5ef7ac4e7b367
OtroDevMas <juliancarrascal365@gmail.com>
2025-02-26 17:18:42 -0500

    Prueba 4

--- Models/conexion.ts
+++ Models/conexion.ts
@@ -3,6 +3,7 @@ import {Client} from "../dependencies/dependencias.ts";
 // Julio Se la come
 // Prueba 2
 // Prueba 3
+// Prueba 4
 
 export const Conexion = await new Client().connect({
     hostname: "localhost",
