a1209a71bca96832474bc64886cb4c336180cec1
gorgojo32 <jpber27@gmail.com>
2025-02-27 09:00:32 -0500

    base1

--- Models/userModels.ts
+++ Models/userModels.ts
@@ -1,3 +1,4 @@
+import { z } from "../dependencies/dependencias.ts";
 import { Conexion } from "./conexion.ts";
 
 
@@ -13,5 +14,19 @@ interface productosData{
 }
 
 export const listarProductos = async()=>{
-    
+    try {
+        const {rows:productos}= await Conexion.execute('');
+        return{
+            success:true,
+            data: productos as productosData[],
+        };
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return{success: false,msg:error.message}
+            
+        } else {
+            return{success: false,msg:"Error en el servidor"}
+        }
+    }
+
 }
