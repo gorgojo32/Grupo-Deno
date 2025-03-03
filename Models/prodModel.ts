eb9105be90bfaa3c6fbf761f568f55bf3c4647d2
gorgojo32 <jpber27@gmail.com>
2025-03-03 17:06:17 -0500

    hola

--- Models/prodModel.ts
+++ Models/prodModel.ts
@@ -12,7 +12,7 @@ interface productosData{
     stock: number;
     imagen: string;
 }
-
+//hola
 export const listarProductos = async()=>{
     try {
         const {rows:productos}= await Conexion.execute('SELECT id_producto,nombre,descripcion,precio,costo,stock,imagen,productos.id_categoria,tipoProducto FROM productos INNER JOIN categorias ON productos.id_categoria = categorias.id_categoria');
